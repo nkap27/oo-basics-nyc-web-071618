7f44df3e43f0287d7eb2bb883231f98fd79b1189
Natasha K <kapr.natasha@gmail.com>
2018-07-18 17:51:01 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+  attr_reader :title, :author, :genre
+  attr_accessor :page_count
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,52 @@
 # Make your shoe class here!
+class Shoe
+  attr_reader :brand, :size, :material, :color
+  attr_accessor :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+
+end
